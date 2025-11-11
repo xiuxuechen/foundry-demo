34959bc48e63b4d29e1587527034150d73f73f1f
xiuxuechen <13206467065@139.com>
2025-11-11 17:40:26 +0800

    study5-10

--- src/HelloWorld.sol
+++ src/HelloWorld.sol
@@ -1,6 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
 contract HelloWorld {
-    
+
+    uint256 public count;
+
+    string public name;
+
+    constructor (uint256 _iniCount, string memory _name){
+        count = _iniCount;
+        name = _name;
+    }
+
+    function incrCount(uint256 _num) public virtual {
+        count = count + _num;
+    }
 }
